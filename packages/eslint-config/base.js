76333999d3cdd300396706a509cf8a0f00c838ed
samuelhulla <oficialny.hulla@gmail.com>
2025-06-07 20:23:22 +0200

    :robot: devops: (repo) setup dx tolling

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,16 +1,18 @@
-import js from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import turboPlugin from "eslint-plugin-turbo";
-import tseslint from "typescript-eslint";
-import onlyWarn from "eslint-plugin-only-warn";
+import js from "@eslint/js"
+import eslintConfigPrettier from "eslint-config-prettier"
+import onlyWarn from "eslint-plugin-only-warn"
+import prettier from "eslint-plugin-prettier/recommended"
+import turboPlugin from "eslint-plugin-turbo"
+import tseslint from "typescript-eslint"
 
 /**
  * A shared ESLint configuration for the repository.
  *
  * @type {import("eslint").Linter.Config[]}
  * */
-export const config = [
+export const config = tseslint.config(
   js.configs.recommended,
+  prettier,
   eslintConfigPrettier,
   ...tseslint.configs.recommended,
   {
@@ -29,4 +31,18 @@ export const config = [
   {
     ignores: ["dist/**"],
   },
-];
+  {
+    rules: {
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  }
+)
